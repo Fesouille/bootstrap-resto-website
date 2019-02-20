00bc1de7bf7a3da1d4a2b1fc3e70daee0f49aec6
Fesouille <ismail.jsph@gmail.com>
2019-02-20 17:09:07 +0100

    local storage

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,25 @@
 if(!sessionStorage.getItem('user_name')) {
-  populateStorage();
+	populateStorage();
 } else {
-  setName();
+	if(sessionStorage.getItem('user_name')=="inconnu(e)"){
+		rePopulateStorage();
+	}
+	setName();
 }
 
 function populateStorage (){
 	var name=prompt("Quel est votre nom?");
 	if(name==="" || name===null){
-		name="inconnu";
+		name="inconnu(e)";
+	}
+	sessionStorage.setItem('user_name', name);
+	setName();
+}
+
+function rePopulateStorage(){
+	var name=prompt("Comment vous appelez autrement que 'inconnu' ?");
+	if(name==="" || name===null){
+		name="étranger(e)";
 	}
 	sessionStorage.setItem('user_name', name);
 	setName();
