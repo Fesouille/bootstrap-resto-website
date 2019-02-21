42812146bbfd198e5254fd23c48118592e32c8c1
Fesouille <ismail.jsph@gmail.com>
2019-02-21 17:58:58 +0100

    Service Worker

---
+++ sw.js
@@ -0,0 +1,41 @@
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open('v1').then(function(cache) {
+      return cache.addAll([
+        '/bootstrap-resto-website/',
+        '/bootstrap-resto-website/offline.html',
+        '/bootstrap-resto-website/assets/css/style.css',
+        '/bootstrap-resto-website/assets/bootstrap/css/bootstrap.css',
+        '/bootstrap-resto-website/assets/bootstrap/css/bootstrap.min.css',
+        '/bootstrap-resto-website/assets/bootstrap/css/bootstrap-grid.min.css',
+        '/bootstrap-resto-website/assets/bootstrap/css/bootstrap-reboot.min.css',
+        '/bootstrap-resto-website/assets/bootstrap/jss/bootstrap.bundle.min.js',
+        '/bootstrap-resto-website/assets/bootstrap/jss/bootstrap.min.js',
+      ]);
+    })
+  );
+});
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(caches.match(event.request).then(function(response) {
+    // caches.match() always resolves
+    // but in case of success response will have value
+    if (response !== undefined) {
+      return response;
+    } else {
+      return fetch(event.request).then(function (response) {
+        // response may be used only once
+        // we need to save clone to put one copy in cache
+        // and serve second one
+        let responseClone = response.clone();
+        
+        caches.open('v1').then(function (cache) {
+          cache.put(event.request, responseClone);
+        });
+        return response;
+      }).catch(function () {
+        return caches.match('/bootstrap-resto-website/assets/img/falafel.jpg');
+      });
+    }
+  }));
+});
