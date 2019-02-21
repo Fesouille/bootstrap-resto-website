2e6a053a41ed1044866b62a19edd8e1346ebea54
Fesouille <ismail.jsph@gmail.com>
2019-02-21 17:37:39 +0100

    Service Worker

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,22 @@
+// register service worker - BEGINNING
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.register('/bootstrap-resto-website/sw.js', { scope: '/bootstrap-resto-website/' }).then(function(reg) {
+
+    if(reg.installing) {
+      console.log('Service worker installing');
+    } else if(reg.waiting) {
+      console.log('Service worker installed');
+    } else if(reg.active) {
+      console.log('Service worker active');
+    }
+
+  }).catch(function(error) {
+    // registration failed
+    console.log('Registration failed with ' + error);
+  });
+}
+// register service worker - END
+
 /*Acceuil Script - BEGINNING*/
 function welcome(){
 	if(!sessionStorage.getItem('user_name')) {
---
+++ sw.js
@@ -0,0 +1,35 @@
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open('v1').then(function(cache) {
+      return cache.addAll([
+        '/bootstrap-resto-website/',
+        '/bootstrap-resto-website/offline.html',
+        '/bootstrap-resto-website/assets/css/style.css'
+      ]);
+    })
+  );
+});
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(caches.match(event.request).then(function(response) {
+    // caches.match() always resolves
+    // but in case of success response will have value
+    if (response !== undefined) {
+      return response;
+    } else {
+      return fetch(event.request).then(function (response) {
+        // response may be used only once
+        // we need to save clone to put one copy in cache
+        // and serve second one
+        let responseClone = response.clone();
+        
+        caches.open('v1').then(function (cache) {
+          cache.put(event.request, responseClone);
+        });
+        return response;
+      }).catch(function () {
+        return caches.match('/bootstrap-resto-website/assets/img/falafel.jpg');
+      });
+    }
+  }));
+});
