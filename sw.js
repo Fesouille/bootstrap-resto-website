5e8853f5402745c5266c3b7f948536465a4a11ae
Fesouille <ismail.jsph@gmail.com>
2019-02-21 18:19:08 +0100

    Service Worker

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.addEventListener('install', function(event) {
   event.waitUntil(
-    caches.open('v1').then(function(cache) {
+    caches.open('v2').then(function(cache) {
       return cache.addAll([
         '/bootstrap-resto-website/',
         '/bootstrap-resto-website/offline.html',
