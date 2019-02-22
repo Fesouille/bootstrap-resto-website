73bd6d4ebd894e219e9213008ff49891f4022263
Fesouille <ismail.jsph@gmail.com>
2019-02-22 14:50:36 +0100

    offline css

--- sw.js
+++ sw.js
@@ -3,8 +3,15 @@ this.addEventListener('install', function(event) {
     caches.open('v2').then(function(cache) {
       return cache.addAll([
         '/bootstrap-resto-website/',
-        '/bootstrap-resto-website/offline/offline.html',
+        '/bootstrap-resto-website/offline/acceuil_offline.html',
+        '/bootstrap-resto-website/offline/carte_offline.html',
+        '/bootstrap-resto-website/offline/gallerie_offline.html',
+        '/bootstrap-resto-website/offline/restaurants_offline.html',
+        '/bootstrap-resto-website/offline/contact_offline.html',
         '/bootstrap-resto-website/offline/offline_style.css',
+        '/bootstrap-resto-website/assets/img/pizza-falafel_logo.png',
+        '/bootstrap-resto-website/assets/img/pizza-falafel_logo.ico',
+        
       ]);
     })
   );
