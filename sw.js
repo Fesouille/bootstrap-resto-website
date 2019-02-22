98219f8a4fb0fbca1afc8e928617a90335260f9b
Fesouille <ismail.jsph@gmail.com>
2019-02-22 16:33:09 +0100

    pwa

--- sw.js
+++ sw.js
@@ -10,7 +10,7 @@ this.addEventListener('install', function(event) {
         '/bootstrap-resto-website/gallerie.html',
         '/bootstrap-resto-website/restaurants.html',
         '/bootstrap-resto-website/contact.html', 
-        '/bootstrap-resto-website/assets/img'
+        '/bootstrap-resto-website/assets/img/'
         
       ]);
     })
